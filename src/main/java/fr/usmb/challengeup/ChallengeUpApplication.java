dba050a68ad4c09846d98fe5a001a5a2346fe329
JulienGln <juliengalerne.jg@gmail.com>
2024-02-10 18:21:27 +0100

    init spring boot

---
+++ src/main/java/fr/usmb/challengeup/ChallengeUpApplication.java
@@ -0,0 +1,13 @@
+package fr.usmb.challengeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/usmb/challengeup/ChallengeUpApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.usmb.challengeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
