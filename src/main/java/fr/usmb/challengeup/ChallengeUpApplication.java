30421c9f5e61fae5f07af2dbe8d936d1c2a0a059
JulienGln <juliengalerne.jg@gmail.com>
2024-02-20 09:27:15 +0100

    Test RestController

--- src/main/java/fr/usmb/challengeup/ChallengeUpApplication.java
+++ src/main/java/fr/usmb/challengeup/ChallengeUpApplication.java
@@ -2,12 +2,21 @@ package fr.usmb.challengeup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ChallengeUpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeUpApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return "Hello " + name + " !";
 	}
 
 }
