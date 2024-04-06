071efdec9b217d12075a2747409683eb3b9a80c0
JulienGln <juliengalerne.jg@gmail.com>
2024-04-06 10:27:16 +0200

    end ChallengeControllerTest (for now)

--- src/test/java/fr/usmb/challengeup/controllers/ChallengeControllerTest.java
+++ src/test/java/fr/usmb/challengeup/controllers/ChallengeControllerTest.java
@@ -2,6 +2,8 @@ package fr.usmb.challengeup.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.usmb.challengeup.entities.Challenge;
+import fr.usmb.challengeup.entities.Progress;
+import fr.usmb.challengeup.entities.User;
 import fr.usmb.challengeup.services.ChallengeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Collections.emptyList;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -45,16 +48,17 @@ public class ChallengeControllerTest {
 
     @Test
     public void getAllChallenges() throws Exception {
+        User user = new User("Toto", "toto@mail.com", "passwordCool*");
         Challenge challenge = new Challenge("Manger", "Sport", Challenge.Periodicity.MENSUEL, "blabla", null);
-        Challenge challenge2 = new Challenge("Boire", "Sport", Challenge.Periodicity.MENSUEL, "blabla", null);
+        Challenge challenge2 = new Challenge("Boire", "Sport", Challenge.Periodicity.MENSUEL, "blabla", user);
         Challenge challenge3 = new Challenge("Dormir", "Sport", Challenge.Periodicity.MENSUEL, "blabla", null);
 
         when(challengeService.getAllChallenges()).thenReturn(List.of(challenge, challenge2, challenge3));
-
         mockMvc.perform(get("/challenge/all"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].title", is("Manger")))
                 .andExpect(jsonPath("$[1].title", is("Boire")))
+                .andExpect(jsonPath("$[1].user.username", is("Toto")))
                 .andExpect(jsonPath("$[2].title", is("Dormir")));
     }
 
@@ -63,9 +67,70 @@ public class ChallengeControllerTest {
         Challenge challenge = new Challenge("Manger", "Sport", Challenge.Periodicity.MENSUEL, "blabla", null);
 
         when(challengeService.getChallengeById(anyLong())).thenReturn(Optional.of(challenge));
-
         mockMvc.perform(get("/challenge/1"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.title", is("Manger")));
+    }
+
+    @Test
+    public void getChallengesByUserId() throws Exception {
+        User user = new User("Toto", "toto@mail.com", "passwordCool*");
+        Challenge challenge = new Challenge("Manger", "Sport", Challenge.Periodicity.MENSUEL, "blabla", user);
+        Challenge challenge2 = new Challenge("Boire", "Sport", Challenge.Periodicity.MENSUEL, "blabla", user);
+        Challenge challenge3 = new Challenge("Dormir", "Sport", Challenge.Periodicity.MENSUEL, "blabla", user);
+
+        when(challengeService.getChallengesByUserId(user.getId())).thenReturn(List.of(challenge, challenge2, challenge3));
+        mockMvc.perform(get("/challenge/user/" + user.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title", is("Manger")))
+                .andExpect(jsonPath("$[1].title", is("Boire")))
+                .andExpect(jsonPath("$[1].user.username", is("Toto")))
+                .andExpect(jsonPath("$[2].title", is("Dormir")));
+    }
+
+    @Test
+    public void getChallengesByUserId_UserDoesNotExist() throws Exception {
+        long nonExistentUserId = 9999L;
+
+        when(challengeService.getChallengesByUserId(nonExistentUserId)).thenReturn(emptyList());
+        mockMvc.perform(get("/challenge/user/" + nonExistentUserId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", is(emptyList())));
+    }
+
+    @Test
+    public void getChallengesByUserId_UserExistsButNoChallenges() throws Exception {
+        User user = new User("Toto", "toto@mail.com", "passwordCool*");
+
+        when(challengeService.getChallengesByUserId(user.getId())).thenReturn(emptyList());
+        mockMvc.perform(get("/challenge/user/" + user.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", is(emptyList())));
+    }
+
+    @Test
+    public void findChallengesWithHighestProgress() throws Exception {
+        User user = new User("Toto", "toto@mail.com", "passwordCool*");
+        Challenge challenge = new Challenge("Manger", "Sport", Challenge.Periodicity.MENSUEL, "blabla", user);
+        Challenge challenge2 = new Challenge("Boire", "Sport", Challenge.Periodicity.MENSUEL, "blabla", user);
+        Challenge challenge3 = new Challenge("Dormir", "Sport", Challenge.Periodicity.MENSUEL, "blabla", user);
+        Progress progress = new Progress(challenge, user);
+        progress.setCompleted(true);
+        Progress progress2 = new Progress(challenge2, user);
+        progress2.setCompleted(false);
+        Progress progress3 = new Progress(challenge3, user);
+        progress3.setCompleted(true);
+
+        when(challengeService.findChallengesWithHighestProgress()).thenReturn(List.of(challenge, challenge3));
+        mockMvc.perform(get("/challenge/highestProgress"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].title", is("Manger")))
+                .andExpect(jsonPath("$[1].title", is("Dormir")));
+
+        // Aucun challenge correspondant
+        when(challengeService.findChallengesWithHighestProgress()).thenReturn(emptyList());
+        mockMvc.perform(get("/challenge/highestProgress"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", is(emptyList())));
     }
 }
