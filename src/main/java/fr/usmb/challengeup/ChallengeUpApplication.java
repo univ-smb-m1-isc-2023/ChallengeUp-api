b0c69ea39b74b6ea4c9aea0895a38e2ccc89f04b
JulienGln <juliengalerne.jg@gmail.com>
2024-04-20 09:21:37 +0200

    add Discord Token to git secrets

--- src/main/java/fr/usmb/challengeup/ChallengeUpApplication.java
+++ src/main/java/fr/usmb/challengeup/ChallengeUpApplication.java
@@ -1,17 +1,36 @@
 package fr.usmb.challengeup;
 
+import fr.usmb.challengeup.bot.DiscordBot;
+import jakarta.annotation.PostConstruct;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.security.auth.login.LoginException;
+
 @SpringBootApplication
 @RestController
 public class ChallengeUpApplication {
+	@Value("${DISCORD_TOKEN:defaultValue}")
+	private String discordToken;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeUpApplication.class, args);
+	}
+
+	@PostConstruct
+	public void startBot() throws LoginException {
+		if (!discordToken.equals("defaultValue")) {
+			JDABuilder.createDefault(discordToken)
+					.enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT)
+					.addEventListeners(new DiscordBot())
+					.build();
+		}
 	}
 
 	@GetMapping("/hello")
